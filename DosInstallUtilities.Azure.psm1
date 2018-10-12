2342038003f68d9ff8bb7be1b302ebe06ae05d8b
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-12 15:10:21 -0700

    remove menu folder items
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- DosInstallUtilities.Azure.psm1
+++ DosInstallUtilities.Azure.psm1
@@ -69,12 +69,6 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 . $PSScriptRoot\functions\KeyVault\GetListOfSecretsInVault.ps1
 . $PSScriptRoot\functions\KeyVault\GetKeyInVault.ps1
 
-# Menus
-. $PSScriptRoot\functions\Menus\ShowMainMenu.ps1
-. $PSScriptRoot\functions\Menus\ShowProductMenu.ps1
-. $PSScriptRoot\functions\Menus\ShowTroubleshootingMenu.ps1
-
-
 # VirtualMachine
 . $PSScriptRoot\functions\VirtualMachine\StartVMsInResourceGroup.ps1
 . $PSScriptRoot\functions\VirtualMachine\StopVMsInResourceGroup.ps1
--- runintegrationtests.ps1
+++ runintegrationtests.ps1
@@ -26,8 +26,8 @@ Import-Module "$here\..\$module\$module.psm1" -Force
 # Invoke-Pester "$here\functions\Storage\GetStorageAccountName.Tests.ps1"
 # Invoke-Pester "$here\functions\Storage\SetStorageAccountNameIntoSecret.Tests.ps1" -Tag 'Unit'
 # Invoke-Pester "$here\functions\Storage\SetStorageAccountNameIntoSecret.Tests.ps1" -Tag 'Integration'
-Invoke-Pester "$here\functions\Storage\CreateAzureStorage.Tests.ps1" -Tag 'Integration' -Verbose
-Invoke-Pester "$here\functions\Storage\InstallStackInKubernetes.Tests.ps1" -Tag 'Integration' -Verbose
+# Invoke-Pester "$here\functions\Storage\CreateAzureStorage.Tests.ps1" -Tag 'Integration' -Verbose
+# Invoke-Pester "$here\functions\Storage\InstallStackInKubernetes.Tests.ps1" -Tag 'Integration' -Verbose
 
 # # Subscription
 # Invoke-Pester "$here\functions\Subscription\SetCurrentAzureSubscription.Tests.ps1" -Tag 'Unit'
@@ -51,7 +51,7 @@ Invoke-Pester "$here\functions\Storage\InstallStackInKubernetes.Tests.ps1" -Tag 
 # Invoke-Pester "$here\functions\arm\DeployTemplate.Tests.ps1" -Tag 'Unit' -Verbose
 # Invoke-Pester "$here\functions\arm\DeployTemplate.Tests.ps1" -Tag 'Cluster' -Verbose
 # Invoke-Pester "$here\functions\arm\DeployTemplate.Tests.ps1" -Tag 'ACS' -Verbose
-# Invoke-Pester "$here\functions\arm\DeployTemplate.Tests.ps1" -Tag 'AKS' -Verbose
+Invoke-Pester "$here\functions\arm\DeployTemplate.Tests.ps1" -Tag 'AKS' -Verbose
 
 # Set-AzureRmContext -SubscriptionId "c8b1589f-9270-46ee-967a-417817e7d10d" -Verbose
 # Get-AzureRmAks
