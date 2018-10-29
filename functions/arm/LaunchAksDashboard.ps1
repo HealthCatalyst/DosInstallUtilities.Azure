45f2c49347c785df51d31399dc15d73ea453d7fd
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-29 11:53:01 -0700

    update version
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- functions/arm/LaunchAksDashboard.ps1
+++ functions/arm/LaunchAksDashboard.ps1
@@ -38,7 +38,7 @@ function LaunchAksDashboard() {
 
     [int[]] $myPortArray = 8001, 8002, 8003, 8004, 8005, 8006, 8007, 8008, 8009, 8010, 8011, 8012, 8013, 8014, 8015, 8016, 8017, 8018, 8019, 8020, 8021, 8022, 8023, 8024, 8025, 8026, 8027, 8028, 8029, 8030, 8031, 8032, 8033, 8034, 8035, 8036, 8037, 8038, 8039
     [int] $port = $(Find-OpenPort -portArray $myPortArray).Port
-    Write-Host "Starting Aks Dashboard on port $port"
+    Write-Host "Starting Aks Dashboard for cluster $clusterName resource group $resourceGroup on port $port"
 
     if ($runAsJob) {
         $sb = [scriptblock]::Create("az aks browse --resource-group $resourceGroup --name $clusterName --listen-port $port")
